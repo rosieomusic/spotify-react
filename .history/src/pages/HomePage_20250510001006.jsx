301f2757f295fb24929d0b02fffec5c99dd73197
rosieomusic <rosieomusic@gmail.com>
2025-05-10 00:11:04 -0400

    first draft ready to deploy

--- .history/src/pages/HomePage_20250510001006.jsx
+++ .history/src/pages/HomePage_20250510001006.jsx
@@ -7,11 +7,10 @@ export default function HomePage() {
 			<p>
 				This little web app is just for fun. <br />
 				It connects to two different APIs: <br />
-				One gives you a random joke.
+				One gives you a random joke <br />
+				The other pulls info from Spotify
 				<br />
-				The other pulls info from Spotify.
-				<br />
-				Work in progess.
+				Work in progess
 			</p>
 		</div>
 	);
